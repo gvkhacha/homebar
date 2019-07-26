38759f836a1a0746ff9a946b7bfa05c6242858b5
Garik Khachatryan <gvkhacha@uci.edu>
2019-07-26 16:54:44 -0700

    basic filter front end

--- src/DrinkList.js
+++ src/DrinkList.js
@@ -20,18 +20,55 @@ const Drink = (props) => {
 
 const DrinkList = () => {
   const [drinks, setDrinks] = useState([]);
-  useEffect(() => {
-    axios.get('http://localhost:3001/available')
+  const [availFilter, setAvailFilter] = useState('available');
+  const [liqFilter, setLiqFilter] = useState('all');
+
+  const getAllDrinks = () => {
+    axios.get("http://localhost:3001/all")
       .then(result => {
-        console.log(result);
-        setDrinks(result.data)
+        setDrinks(result.data);
       })
-  }, [])
+      setAvailFilter('all');
+  }
+
+  const getAvailableDrinks = () => {
+    axios.get("http://localhost:3001/available")
+      .then(result => {
+        setDrinks(result.data);
+      })
+      setAvailFilter('available');
+  }
+
+  const filterHandler = (e) => {
+    setLiqFilter(e.target.value);
+  }
+
+
+  useEffect(getAvailableDrinks, [])
 
   return (
     <div>
-      <h1>You can make</h1>
-      {drinks.map(d => <Drink key={d.name} name={d.name} imageUrl={d.imageUrl}/>)}
+      <form>
+        <label><input type="radio" value="all" 
+            checked={availFilter === 'all'} onChange={() => getAllDrinks()} /> All Drinks</label>
+        <label><input type="radio" value="available" 
+            checked={availFilter === 'available'} onChange={() => getAvailableDrinks()}/> Available Drinks</label>
+      </form>
+      <form>
+        <label><input type="radio" value="all" checked={liqFilter === 'all'} onChange={(e) => filterHandler(e)} /> All Liquor</label>
+        <label><input type="radio" value="vodka" checked={liqFilter === 'vodka'} onChange={(e) => filterHandler(e)}/> Vodka </label>
+        <label><input type="radio" value="tequila" checked={liqFilter === 'tequila'} onChange={(e) => filterHandler(e)}/> Tequila </label>
+        <label><input type="radio" value="cognac" checked={liqFilter === 'cognac'} onChange={(e) => filterHandler(e)}/> Cognac </label>
+        <label><input type="radio" value="whiskey" checked={liqFilter === 'whiskey'} onChange={(e) => filterHandler(e)}/> Whiskey </label>
+        <label><input type="radio" value="gin" checked={liqFilter === 'gin'} onChange={(e) => filterHandler(e)}/> Gin </label>
+        <label><input type="radio" value="rum" checked={liqFilter === 'rum'} onChange={(e) => filterHandler(e)}/> Rum </label>
+      </form>
+      {drinks.filter((drink) => {
+        if(liqFilter === 'all'){
+          return true;
+        }
+        return drink.ingredients.find(ing => ing.name.toLowerCase().includes(liqFilter))
+      }).map(d => <Drink key={d.name} name={d.name} imageUrl={d.imageUrl}/>)}
     </div>
     )
 }
