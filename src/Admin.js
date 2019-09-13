1465a413de088eefe4d76c749c255815ce75c882
Garik Khachatryan <gvkhacha@uci.edu>
2019-09-12 22:21:29 -0700

    adding fabric ui, new admin page

---
+++ src/Admin.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
+import { Stack } from 'office-ui-fabric-react/lib/Stack';
+import { Dropdown } from 'office-ui-fabric-react/lib/Dropdown';
+import { Label } from 'office-ui-fabric-react/lib/Label';
+import { ComboBox } from 'office-ui-fabric-react/lib/index';
+
+
+const Ingredient = ({ name }) => {
+    return (
+        <Label>{name}</Label>
+    )
+}
+
+const glassOptions = [
+    { key: 'rocks', text: 'Rocks Glass'},
+    { key: 'highball', text: 'Highball'},
+    { key: 'martini', text: 'Martini'},
+    { key: 'shot', text: 'Shot Glass'},
+    { key: 'coupe', text: 'Coupe'},
+    { key: 'mule', text: 'Mule Mug'}
+];
+
+const Admin = () => {
+    const [name, setName] = useState("Old Fashioned");
+    const [url, setUrl] = useState("https://cdn.liquor.com/wp-content/uploads/2018/05/08113350/bourbon-old-fashioned-720x720-recipe.jpg");
+    const [instructions, setInstructions] = useState('');
+    const [ingredients, setIngredients] = useState([]);
+    const [allIngr, setAllIngr] = useState([]);
+
+    const columnProps = {
+        tokens: { childrenGap: 15 },
+        styles: { root: { width: 300 } }
+    };
+
+    const onIngrChange = (ev, op, index, value) => {
+        if(value !== undefined){
+            const newIng = {key: value, text: value};
+            setAllIngr(allIngr.concat([newIng]));
+        }else if(op != undefined){
+            if(op.selected){
+                setIngredients(ingredients.concat([op]));
+            }else{
+                setIngredients(ingredients.filter((i) => i.key !== op.key));
+            }
+        }
+    }
+
+    return (
+        <div className="newDrink">
+            <form id="newDrinkForm">
+                <Stack {...columnProps}>
+                    <TextField label="Drink Name" value={name} onChange={(e, v) => setName(v)}/>
+                    <TextField label="Image URL" value={url} onChange={(e, v) => setUrl(v)} />
+                    <Dropdown
+                        label="Glassware"
+                        defaultSelectedKey="rocks"
+                        options={glassOptions}
+                    />
+                    <TextField label="Instructions" multiline rows={3} value={instructions} onChange={(e, v) => setInstructions(v)} />
+                    <Label>Ingredients</Label>
+                    <ComboBox
+                        multiSelect
+                        label="Add Ingredient"
+                        allowFreeform={true}
+                        autoComplete={'on'}
+                        options={allIngr}
+                        onChange={onIngrChange}
+                        // options={allIngredients}
+                        // onChange={(e, o, i, v) => console.log(o, i, v)}
+                    />
+                    {ingredients.sort((a, b) => a.name <= b.name ? -1 : 1)
+                        .map(ing => 
+                            <Ingredient key={ing.key} name={ing.text} />)}
+                </Stack>
+            </form>
+        </div>
+    )
+}
+
+
+export default Admin;
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import IngredientsList from './IngredientsList';
+import Admin from './Admin';
 import DrinkList from './DrinkList';
 import { Switch, Route } from 'react-router-dom'
 
@@ -8,7 +9,7 @@ const App = () => {
   return (
     <Switch>
       <Route exact path='/' component={DrinkList}/>
-      <Route path='/admin' component={IngredientsList}/>
+      <Route path='/admin' component={Admin}/>
     </Switch>
     )
 }
